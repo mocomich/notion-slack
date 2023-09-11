e433c2c447d7c660f46872c70862acfe5ed2fb38
mk <mk.eng1119@gmail.com>
2023-09-11 18:53:00 +0900

    add: for development

--- src/worker.ts
+++ src/worker.ts
@@ -1,13 +1,32 @@
+import { Hono } from 'hono';
 import { scheduleHandler } from './handler/scheduleHandler';
 
-export interface Env {
+export type Env = {
 	NOTION_TOKEN: string;
 	NOTION_DATABASE_ID: string;
 	SLACK_TOKEN: string;
+};
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const app = new Hono<{ Bindings: Env }>();
+
+// debug
+if (isDevelopment) {
+	app.get('/_/debug', async (c) => {
+		try {
+			await scheduleHandler(c.env);
+			return c.text('ok');
+		} catch (e) {
+			console.error(e);
+			return c.text('error', 500);
+		}
+	});
 }
 
 export default {
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
 		ctx.waitUntil(scheduleHandler(env));
 	},
+	fetch: app.fetch,
 };
